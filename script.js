c545f07c14ec8af68f14d1769bbb4bb275765e0a
LEVELUP002 <julienmg3@gmail.com>
2026-09-24 14:46:07 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,4 @@
+document.getElementById("year").textContent = new Date().getFullYear();
+function notify(name){
+  alert(name + " — mbola demo. Ampidiro eto ny rohy download rehefa vonona ilay logiciel.");
+}
